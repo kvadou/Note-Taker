e6bf327ed1653c0369d9ed25702e4cb330bcd640
DPK <dougkvamme@gmail.com>
2020-12-12 12:13:22 -0600

    updated api-routes

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,20 +1,41 @@
-// Dependencies
-const express = require("express");
 const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-// Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 8080;
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(data);
+  });
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use("/assets", express.static("./assets"));
+  app.get("/api/notes/:id", function (req, res) {
+    res.json(data[Number(req.params.id)]);
+  });
 
-require("./routing/html-routes")(app);
-require("./routing/api-routes")(app);
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let uniqueId = data.length.toString();
+    console.log(uniqueId);
+    newNote.id = uniqueId;
+    data.push(newNote);
 
-// Starts the server to begin listening
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), function (err) {
+      if (err) throw err;
+    });
+
+    res.json(data);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    let noteId = req.params.id;
+    let newId = 0;
+    console.log(`Deleting note with id ${noteId}`);
+    data = data.filter((currentNote) => {
+      return currentNote.id != noteId;
+    });
+    for (currentNote of data) {
+      currentNote.id = newId.toString();
+      newId++;
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    res.json(data);
+  });
+};
